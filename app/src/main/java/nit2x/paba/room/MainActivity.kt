c9073601858b74ab8105ed6b1ed5aabfd0a9f6be
Nama Anda <hermawanedbert@gmail.com>
2024-12-27 16:46:57 +0700

    Commit 2

--- app/src/main/java/nit2x/paba/room/MainActivity.kt
+++ app/src/main/java/nit2x/paba/room/MainActivity.kt
@@ -1,12 +1,22 @@
 package nit2x.paba.room
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import nit2x.paba.room.database.daftarBelanjaDB
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var DB : daftarBelanjaDB
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +25,20 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        DB = daftarBelanjaDB.getDatabase(this)
+        val _fabAdd = findViewById<FloatingActionButton>(R.id.fabAdd)
+        _fabAdd.setOnClickListener {
+            startActivity(Intent(this, TambahDaftar::class.java))
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        CoroutineScope(Dispatchers.Main).async {
+            val daftarBelanja = DB.fundaftarBelanjaDAO().selectAll()
+            Log.d("data ROOM", daftarBelanja.toString())
         }
     }
 }
---
+++ app/src/main/java/nit2x/paba/room/TambahDaftar.kt
@@ -0,0 +1,49 @@
+package nit2x.paba.room
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import nit2x.paba.room.database.daftarBelanja
+import nit2x.paba.room.database.daftarBelanjaDB
+import nit2x.paba.room.helper.DateHelper.getCurrentDate
+
+class TambahDaftar : AppCompatActivity() {
+
+    var DB = daftarBelanjaDB.getDatabase(this)
+    var tanggal = getCurrentDate()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tambah_daftar)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val _etItem = findViewById<EditText>(R.id.etItem)
+        val _etJumlah = findViewById<EditText>(R.id.etJumlah)
+        val _btnTambah = findViewById<Button>(R.id.btnTambah)
+        val _btnUpdate = findViewById<Button>(R.id.btnUpdate)
+
+        _btnTambah.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).async {
+                DB.fundaftarBelanjaDAO().insert(
+                    daftarBelanja(
+                        tanggal = tanggal,
+                        item = _etItem.text.toString(),
+                        jumlah = _etJumlah.text.toString()
+                    )
+                )
+            }
+        }
+    }
+}
